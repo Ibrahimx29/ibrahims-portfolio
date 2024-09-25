9603725db2f31f82cdb88f89f7dc09d9b748af72
Ibrahim <61193755+Ibrahimx29@users.noreply.github.com>
2024-09-25 21:31:07 +0700

    typo

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -18,7 +18,7 @@ function Projects() {
           p="Hamburger Restaurant"
         />
         <ProjectCard src={hipsster} link="#" h3="Hipsster" p="Glasses Shop" />
-        <ProjectCard src={fitLift} link="#" h3="FitList" p="Fitness App" />
+        <ProjectCard src={fitLift} link="#" h3="FitLift" p="Fitness App" />
       </div>
     </section>
   );
