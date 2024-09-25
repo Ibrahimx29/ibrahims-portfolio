2f83559ab2eebdbbac68e1ecfba59f22dcc0960a
Ibrahim <61193755+Ibrahimx29@users.noreply.github.com>
2024-09-25 20:23:02 +0700

    fix responsiveness on skills section and add responsiveness on contact section
    fix responsiveness on skills section and add responsiveness on contact section

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,8 +1,14 @@
 import styles from "./SkillsStyles.module.css";
-import checkMarkIcon from "../../assets/checkmark-dark.svg";
+import checkMarkIconDark from "../../assets/checkmark-dark.svg";
+import checkMarkIconLight from "../../assets/checkmark-light.svg";
 import SkillList from "../../common/SkillList";
+import { useTheme } from "../../common/ThemeContext";
 
 function Skills() {
+  const { theme } = useTheme();
+  const checkMarkIcon =
+    theme === "light" ? checkMarkIconLight : checkMarkIconDark;
+
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skill</h1>
@@ -12,12 +18,16 @@ function Skills() {
         <SkillList skill="JavaScript" src={checkMarkIcon} />
         <SkillList skill="TypeScript" src={checkMarkIcon} />
         <SkillList skill="Node" src={checkMarkIcon} />
-        <hr />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
         <SkillList skill="React" src={checkMarkIcon} />
         <SkillList skill="Angular" src={checkMarkIcon} />
         <SkillList skill="Vue" src={checkMarkIcon} />
         <SkillList skill="Tailwind CSS" src={checkMarkIcon} />
-        <hr />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
         <SkillList skill="Redux" src={checkMarkIcon} />
         <SkillList skill="Webpack" src={checkMarkIcon} />
         <SkillList skill="Git" src={checkMarkIcon} />
